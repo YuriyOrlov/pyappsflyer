4a1b15d1eedffcf22b00c807c0692fe930e60e46
YuriyOrlov <navuchodonsr@yandex.ru>
2019-07-18 11:24:18 +0300

    fixed dependencies in requirements
    FIX

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='pyappsflyer',
-      version='0.1',
+      version='0.2',
       description='Unofficial python wrap for AppsFlyer API',
       long_description=long_description,
       long_description_content_type="text/markdown",
@@ -15,6 +15,8 @@ setup(name='pyappsflyer',
       packages=['pyappsflyer'],
       install_requires=[
           'furl==2.0.0',
-          'environs==4.2.0'
+          'environs==4.2.0',
+          'requests==2.22.0',
+          'xmltodict==0.12.0'
       ],
       zip_safe=False)
